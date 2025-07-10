f551a8bf9188527d4ec1d05c7715f4ba3dfb915c
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2025-07-10 21:00:22 +0200

    :sparkles: start coding Hero comp

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import { DisplayText, DisplayTextM, ParagraphP2 } from "@/components/Typography";
+// import SocialLink from "@/components/SocialLink";
+
+export default function Hero() {
+  return (
+    <section className="container px-28 mt-[60px] relative flex items-center justify-between gap-12">
+      
+      {/* Left side */}
+      <div className="max-w-[600px]">
+        <div className="flex flex-col gap-2">
+            <DisplayText level={1} weight="regular">
+            Hello I’am <strong className="font-bold">Sebastian.</strong>
+            </DisplayText>
+            <DisplayText level={2} weight="extrabold">
+                <span>Web </span>
+                <span className="text-outline">Developer</span>
+            </DisplayText>
+            <DisplayText level={2} weight="regular">
+            Based In <strong className="font-bold">Poland.</strong>
+            </DisplayText>
+        </div>
+
+        <ParagraphP2 className="text-[var(--zinc-500)] mt-8">
+            I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to specimen book.
+        </ParagraphP2>
+      </div>
+
+      {/* Right side */}
+      <div className="relative flex-1 -ml-20 lg:-ml-40 xl:-ml-56">
+        <Image
+        src="/images/Banner.svg"
+        alt="Hero Illustration"
+        width={500}
+        height={500}
+        className="w-full h-auto"
+        priority
+        />
+      </div>
+    </section>
+  );
+}
